b7c8894c11897fea087acb34dec8da0d367f5487
Jefferson Lima <100821993+Jeff-Lima94@users.noreply.github.com>
2022-08-05 10:48:44 -0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,37 @@
+import React, { Component } from "react";
+
+export default class Desafio extends Component {
+  state = {
+    num: 0
+  };
+
+  add = () => {
+    this.setState({ num: this.state.num + 1 });
+  };
+
+  remove = () => {
+    this.setState({ num: this.state.num - 1 });
+  };
+
+  render() {
+    return (
+      <>
+        <h1>{this.state.num}</h1>
+        <button
+          onClick={() => {
+            this.add();
+          }}
+        >
+          +
+        </button>
+        <button
+          onClick={() => {
+            this.remove();
+          }}
+        >
+          -
+        </button>
+      </>
+    );
+  }
+}
